bb4e53ca5f69f478386b7c8dc28c0a3a64d01219
Rick Eyre <rick.eyre@hotmail.com>
2014-04-19 18:38:39 -0400

    Rename Map to CommandMapper.

---
+++ test/command-mapper.js
@@ -0,0 +1,30 @@
+// The MIT License (MIT)
+// Copyright (c) 2014 map contributors
+
+var expect = require("chai").expect,
+    CommandMapper = require("../lib/command-mapper"),
+    mapping = require("./mapping.json"),
+    commandMap = new CommandMapper(mapping);
+
+suite("CommandMapper", function() {
+
+  test("should look like a CommandMapper object", function(){
+    expect(CommandMapper).to.have.property("triangulate");
+    expect(commandMap).to.have.property("triangulate");
+  });
+
+  test("should accept an empty string", function() {
+    expect(commandMap.triangulate("")).to.equal("");
+  });
+
+
+  test("CommandMapper constructor should not accept an array", function() {
+    expect(function() { new CommandMapper([]) }).to.throw(Error);
+  });
+
+
+  test("should have two mappings", function() {
+    expect(commandMap.mappings).to.have.length(2);
+  });
+
+});
